0ffb2e2d1e802a673e02e4dbe401df11036404af
hossam-khalaf <hossamhassan303089@gmail.com>
2022-02-13 20:47:56 +0400

    active class

--- src/Filter.js
+++ src/Filter.js
@@ -20,11 +20,23 @@ const Filter = ({
 
 	return (
 		<div className='filter-container'>
-			<button onClick={() => setActiveGenre(0)}>All</button>
+			<button
+				className={activeGenre === 0 ? 'active' : ''}
+				onClick={() => setActiveGenre(0)}>
+				All
+			</button>
 			{/* 35 is id of comedy genre => from the API */}
-			<button onClick={() => setActiveGenre(35)}>Comedy</button>
+			<button
+				className={activeGenre === 35 ? 'active' : ''}
+				onClick={() => setActiveGenre(35)}>
+				Comedy
+			</button>
 			{/* 28 is id of action genre => from the API */}
-			<button onClick={() => setActiveGenre(28)}>Action</button>
+			<button
+				className={activeGenre === 28 ? 'active' : ''}
+				onClick={() => setActiveGenre(28)}>
+				Action
+			</button>
 		</div>
 	)
 }
