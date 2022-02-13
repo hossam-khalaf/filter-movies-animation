2ce16da86f1f976829f33dfe5470073047aff526
hossam-khalaf <hossamhassan303089@gmail.com>
2022-02-13 19:03:34 +0400

    calling API && state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+	const [popular, setPopular] = useState([])
+
+	useEffect(() => {
+		fetchPopularMovies()
+	}, [])
+
 	const fetchPopularMovies = async () => {
-		// https://api.themoviedb.org/3/movie/popular?api_key=1fe844e090d2aab7a7976a3cbfa5d31b&language=en-US&page=1
+		const url =
+			'https://api.themoviedb.org/3/movie/popular?api_key=1fe844e090d2aab7a7976a3cbfa5d31b&language=en-US&page=1'
+
+		const data = await fetch(url)
+		const movies = await data.json()
+		setPopular(movies.results)
 	}
 
 	return (
